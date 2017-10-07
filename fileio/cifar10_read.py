cb7447db2e92fa731bd62b76233ac50f07e23e46
Ashutosh <30094761+agojiya@users.noreply.github.com>
2017-10-07 15:14:29 -0400

    Reshaping to separate width, height, and channel. Previewing images and labels.

--- fileio/cifar10_read.py
+++ fileio/cifar10_read.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 BASE_DIR = 'X:/cifar10'
-FILES = ['batches.meta', 'data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
+FILES = ['batches.meta', 'data_batch_1', 'data_batch_2', 'data_batch_3',
+         'data_batch_4', 'data_batch_5', 'test_batch']
 
 
 def unpickle(file):
@@ -10,32 +11,69 @@ def unpickle(file):
     """
     import pickle
     with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+        dictionary = pickle.load(fo, encoding='bytes')
+    return dictionary
 
 
 batches_meta_unpickled = unpickle(BASE_DIR + '/' + FILES[0])
 NUM_CASES_PER_BATCH = batches_meta_unpickled[b'num_cases_per_batch']
-LABEL_NAMES = batches_meta_unpickled[b'label_names']
+LABEL_NAMES = [s.decode('UTF-8') for s in
+               batches_meta_unpickled[b'label_names']]
 IMAGE_WIDTH = IMAGE_HEIGHT = 32
 
 
 def get_data(train_set=True):
     """
-    Provides a dictionary containing both the images (RGB) and their respective labels. Elements in 'images' are
-    shaped: (<num_images>, 3, 1024) where 3 represents the color channels and 1024 represents intensity values for each
+    Provides a dictionary containing both the images (RGB) and their respective
+    labels. Elements in 'images' are shaped: (<num_images>, 3, 1024) where 3
+    represents the color channels and 1024 represents intensity values for each
     pixel in the respective channel (32 * 32 = 1024).
 
-    :param train_set: Boolean indicating whether the training data should be loaded (True) or the test data (False).
-    :return: A dictionary ('labels', 'images') where the nth label corresponds to the nth image.
+    :param train_set: Boolean indicating whether the training data should be
+        loaded (True) or the test data (False).
+    :return: A dictionary ('labels', 'images') where the nth label corresponds
+        to the nth image.
     """
-    unpickled_target_files = [unpickle(BASE_DIR + '/' + file) for file in (FILES[1:6] if train_set else FILES[6:])]
+    unpickled_target_files = [unpickle(BASE_DIR + '/' + file) for file in
+                              (FILES[1:6] if train_set else FILES[6:])]
 
     output = {'labels': [],
-              'images': np.empty(shape=(NUM_CASES_PER_BATCH, 3, IMAGE_WIDTH * IMAGE_HEIGHT), dtype=np.uint8)}
+              'images': np.empty(
+                  shape=(NUM_CASES_PER_BATCH, IMAGE_WIDTH, IMAGE_HEIGHT, 3),
+                  dtype=np.uint8)}
     for unpickled_file in unpickled_target_files:
         output['labels'] += unpickled_file[b'labels']
-        output['images'] += np.reshape(unpickled_file[b'data'], (NUM_CASES_PER_BATCH, 3, IMAGE_WIDTH * IMAGE_HEIGHT))
+
+        current_images = unpickled_file[b'data']
+        current_images_reshaped = current_images.reshape((NUM_CASES_PER_BATCH,
+                                                          IMAGE_WIDTH,
+                                                          IMAGE_HEIGHT, 3),
+                                                         order='F')
+        output['images'] = np.append(output['images'],
+                                     current_images_reshaped, axis=0)
+    output['images'] = np.delete(output['images'], np.s_[:10000], 0)
+
+    # Images are sideways (maybe a byproduct of fortran-like order in the
+    # reshape?)
+    output['images'] = np.rot90(output['images'], k=-1, axes=(1, 2))
     return output
 
+
 # Todo: Preview images to make sure they're loading correctly
+if __name__ == "__main__":
+    data = get_data(train_set=True)
+
+    import cv2
+    from random import randrange
+
+    while True:
+        index = randrange(0, stop=len(data['labels']))
+        print(LABEL_NAMES[data['labels'][index]])
+
+        bigger_image = np.repeat(np.repeat(data['images'][index], 4, axis=0),
+                                 4, axis=1)
+        cv2.imshow('test', cv2.cvtColor(bigger_image, cv2.COLOR_RGB2BGR))
+
+        # Any key to show another random image, space-bar to exit
+        if cv2.waitKey() & 0xFF == 32:
+            break
